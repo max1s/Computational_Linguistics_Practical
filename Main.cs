105a3871e0f6ecf837f98c15b9c327d0f3cf9eec
Max Whitby <max.whitby@keble.ox.ac.uk>
2015-02-11 09:48:17 +0000

    Here are the origins of the project
    For Omer Farukhan

---
+++ Main.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CLP
+{
+	public class Root
+	{
+		static int Main(string[] args)
+		{
+			Parser parser = new Parser ();
+			parser.ParseText ();
+			return 0;
+		}
+	}
+}
+
---
+++ Parser.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace CLP
+{
+	public class Parser
+	{
+
+		struct Key 
+		{
+			public readonly string myWord;
+			public readonly string myPosTag;
+			public Key(string word, string posTag) 
+			{
+				myWord = word;
+				myPosTag = posTag;
+			}
+		
+		}
+
+		public Parser ()
+		{
+		}
+
+
+
+		public MultiKeyDictionary<string, string, float> ParseText()
+		{
+			var mkd = new MultiKeyDictionary<string, string, float> ();
+			var fileRoot = @"C:\Users\Max\Documents\treeBank\treeBank";
+			var directory = new DirectoryInfo(fileRoot);
+
+			foreach(var subDirectory in directory.GetDirectories())
+			{
+
+				foreach(var file in subDirectory.GetFiles())
+				{
+					var lines = File.ReadAllLines (file.FullName);
+					foreach (string line in lines)
+					{
+						var word = "";
+						var posTag = "";
+						var whichOne = false;
+
+						foreach (char c in line)
+						{
+
+							if (c == '['|| c == ']' ||c == ' ' ||c == '\u0039' ||  c == '\u0044' ||  c == '=' && !whichOne)
+								continue;
+							if (c == ' ' && whichOne)
+							{
+								whichOne = !whichOne;
+							}
+							if (c == '/')
+							{
+								whichOne = !whichOne;
+								continue;
+							}
+							if (!whichOne)
+								word += c;
+							if (whichOne)
+								posTag += c;
+						}
+
+						if (mkd.ContainsKey (word) && mkd.ContainsKey (posTag))
+						{
+
+						}
+
+					}
+						
+				}
+			}
+
+			return mkd;
+		}
+	}
+}
+
